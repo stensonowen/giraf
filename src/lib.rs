d5a6deeff768b16cac3669b0c8d0d821cefeddc8
owen <owen@oms.sh>
2018-03-27 01:28:37 -0400

    use unsafe strat to look up vertices without owning them / rental

--- src/lib.rs
+++ src/lib.rs
@@ -1,176 +1,142 @@
-#![allow(unused)]
-
-/*
- * proper way to store edges? given that they're probably non-unique?
- *  maybe just only insert into AddrHS if absent?
- *  maybe use a vector and just push everything?
- *      never need to look up an edge by its weight or anything
- */
 
 use std::fmt;
-use std::collections::HashSet;
+use std::mem;
+use std::hash::Hash;
+use std::marker::PhantomData;
 use std::collections::HashMap;
 
-mod edge;
-use edge::{Edge};
-use edge::{EdgeDir, DirectedEdge, UndirectedEdge};
-//use edge::{EdgeWeight, SignedEdge, UnsignedEdge, UnweightedEdge};
-use edge::{EdgeWeight, UnweightedEdge};
+///////////////////////////////////////////////////////////////////////////////
+//  Direction
+///////////////////////////////////////////////////////////////////////////////
 
-mod vertex;
-use vertex::{Vertex, NodeT};
+pub trait DirT<E: EdgeT>: fmt::Debug {
+    fn new() -> Self;
+}
 
-mod addr_collec;
-use addr_collec::{AddrSet, VertAddr, EdgeAddr};
+#[derive(Debug)] 
+struct Dir<V: NodeT, E: EdgeT> {
+    children: Vec<*const Edge<V, E, Dir<V, E>>>,
+    parents: Vec<*const Edge<V, E, Dir<V, E>>>,
+}
+
+#[derive(Debug)] 
+struct Undir<V: NodeT, E: EdgeT> {
+    neighbors: Vec<*const Edge<V, E, Undir<V,E>>>,
+}
+
+impl<V: NodeT, E: EdgeT> DirT<E> for Dir<V,E> {
+    fn new() -> Self { Dir { children: vec![], parents: vec![] } }
+}
+impl<V: NodeT, E: EdgeT> DirT<E> for Undir<V,E> {
+    fn new() -> Self { Undir { neighbors: vec![] } }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//  Vertex
+///////////////////////////////////////////////////////////////////////////////
+
+pub trait NodeT: 'static + fmt::Debug + Eq + Hash {}
+impl<T: 'static + fmt::Debug + Eq + Hash> NodeT for T {}
 
 #[derive(Debug)]
-pub struct Graph<V: NodeT, D: EdgeDir, W: EdgeWeight> {
-    // consider `State` element: can store counter if in building state
-    nodes: AddrSet<Vertex<V,D>, VertAddr>,
-    edges: AddrSet<Edge<D,W>, EdgeAddr>,
-
+struct Vertex<V: NodeT, E: EdgeT, D: DirT<E>> {
+    val: Box<V>,
+    hood: D,
+    _e: ::std::marker::PhantomData<E>,
 }
 
-impl<V: NodeT, D: EdgeDir, W: EdgeWeight> Graph<V,D,W> {
-    pub fn new() -> Self {
-        Graph {
-            nodes: AddrSet::default(),
-            edges: AddrSet::default(),
+impl<V: NodeT, E: EdgeT, D: DirT<E>> Vertex<V,E,D> {
+    fn new(val: V) -> Self {
+        Vertex {
+            val: Box::new(val),
+            hood: D::new(),
+            _e: PhantomData,
         }
     }
-    /// A vertex can be added to any graph, 
-    /// regardless of edge weighted-ness or directed-ness
-    pub fn add_vertex(&mut self, val: V) -> &Vertex<V,D> {
-        let vertex = Vertex::new(val);
-        let addr = self.nodes.insert(vertex).unwrap();
-        &self.nodes[addr]
-    }
-
-    pub fn contains_value(&self, val: &V) -> bool {
-        self.nodes.contains(val)
-    }
-    pub fn contains_vertex(&self, val: &Vertex<V,D>) -> bool {
-        self.nodes.contains(val)
+    fn borrow(&self) -> &V {
+        &self.val
     }
 }
 
-// ******************************************************************
-// **********          Unweighted                          **********
-// ******************************************************************
-impl<V: NodeT, D: EdgeDir> Graph<V, D, UnweightedEdge> {
-    // returns edge reference or None if nothing was added (invalid input)
-    // inputs must have `self` lifetime, so they're valid unless deleted
-    // can't just take V as input: would need to create Vertex<&V> :/
-    /*
-    pub fn add_edge<'a>(&'a mut self, l: &'a Vertex<V,D>, r: &'a Vertex<V,D>)
-        -> Option<EdgeAddr> 
-    {
-        let lhs = self.nodes.get(l)?;
-        let rhs = self.nodes.get(r)?;
-        let edge = Edge::between(lhs, rhs, ());
-        let edge_addr = self.edges.insert(edge)?;
-        self.nodes[lhs].register_child_edge(edge_addr);
-        self.nodes[rhs].register_parent_edge(edge_addr);
-        Some(edge_addr)
-    }
-    */
+///////////////////////////////////////////////////////////////////////////////
+//  Edge
+///////////////////////////////////////////////////////////////////////////////
+
+pub trait EdgeT: fmt::Debug {}
+impl<T: fmt::Debug> EdgeT for T {}
+
+#[derive(Debug)]
+struct Edge<V: NodeT, E: EdgeT, D: DirT<E>> {
+    val: E,
+    lhs: *const Vertex<V,E,D>,
+    rhs: *const Vertex<V,E,D>,
 }
 
-
-// ******************************************************************
-// **********          Undirected                          **********
-// ******************************************************************
-impl<V: NodeT, W: EdgeWeight> Graph<V, UndirectedEdge, W> {
-
-}
-
-// ******************************************************************
-// **********          Undirected, Unweighted              **********
-// ******************************************************************
-
-impl<V: NodeT> Graph<V, UndirectedEdge, UnweightedEdge> {
-    pub fn new_undirected_unweighted() -> Self {
-        Self::new()
-    }
-    pub fn add_edge<'a>(&'a mut self, 
-                        l: &'a Vertex<V, UndirectedEdge>, 
-                        r: &'a Vertex<V, UndirectedEdge>) 
-        -> Option<EdgeAddr> 
-    {
-        let lhs = self.nodes.get(l)?;
-        let rhs = self.nodes.get(r)?;
-        let edge = Edge::between(lhs.clone(), rhs.clone(), UnweightedEdge);
-        let edge_addr = self.edges.insert(edge)?;
-        self.nodes[lhs].register_neighbor(edge_addr.clone());
-        self.nodes[rhs].register_neighbor(edge_addr.clone());
-        Some(edge_addr)
+impl<V: NodeT, E: EdgeT, D: DirT<E>> Edge<V,E,D> {
+    fn new(e: E, l: &Vertex<V,E,D>, r: &Vertex<V,E,D>) -> Self {
+        Edge {
+            val: e,
+            lhs: l as *const Vertex<V,E,D>,
+            rhs: r as *const Vertex<V,E,D>,
+        }
     }
 }
+impl<V: NodeT, E: EdgeT> Edge<V, E, Dir<V,E>> { }
+impl<V: NodeT, E: EdgeT> Edge<V, E, Undir<V,E>> { }
 
-// ******************************************************************
-// **********          Directed, Unweighted                **********
-// ******************************************************************
-impl<V: NodeT> Graph<V, DirectedEdge, UnweightedEdge> {
-    pub fn new_directed_unweighted() -> Self {
-        Self::new()
+///////////////////////////////////////////////////////////////////////////////
+//  Graph
+///////////////////////////////////////////////////////////////////////////////
+
+#[derive(Debug)]
+pub struct Graph<V: NodeT, E: EdgeT, D: DirT<E>> {
+    nodes: HashMap<&'static V, Vertex<V,E,D>>,
+    edges: Vec<Box<Edge<V,E,D>>>,
+}
+
+impl<V: NodeT, E: EdgeT, D: DirT<E>> Graph<V,E,D> {
+    pub fn new() -> Self { Graph { nodes: HashMap::new(), edges: Vec::new(), } }
+    pub fn with_capacity(n: usize, m: usize) -> Self {
+        Graph { nodes: HashMap::with_capacity(n), edges: Vec::with_capacity(m) }
     }
-    pub fn add_edge<'a>(&'a mut self, 
-                        l: &'a Vertex<V, DirectedEdge>, 
-                        r: &'a Vertex<V, DirectedEdge>) 
-        -> Option<EdgeAddr> 
-    {
-        let lhs = self.nodes.get(l)?;
-        let rhs = self.nodes.get(r)?;
-        let edge = Edge::between(lhs.clone(), rhs.clone(), UnweightedEdge);
-        let edge_addr = self.edges.insert(edge)?;
-        self.nodes[lhs].register_child(edge_addr.clone());
-        self.nodes[rhs].register_parent(edge_addr.clone());
-        Some(edge_addr)
+    pub fn insert_vertex(&mut self, v: V) {
+        assert!(self.nodes.contains_key(&v) == false);
+        let vert = Vertex::new(v);
+        let val = unsafe {
+            mem::transmute::<&V, &'static V>(vert.borrow())
+        };
+        self.nodes.insert(val, vert);
+    }
+    fn get_vertex(&self, v: &V) -> Option<&Vertex<V,E,D>> {
+        self.nodes.get(v)
+    }
+    fn create_edge(&mut self, e: E, l: &V, r: &V) -> Option<*const Edge<V,E,D>> {
+        let edge = {
+            let lhs = self.get_vertex(l)?;
+            let rhs = self.get_vertex(r)?;
+            Edge::new(e, lhs, rhs)
+        };
+        self.edges.push(Box::new(edge));
+        let edge: &Edge<V,E,D> = &self.edges.last().unwrap();
+        Some(edge as *const Edge<V,E,D>)
     }
 }
 
-/*
-// ******************************************************************
-// **********          Directed, Unsigned Weights          **********
-// ******************************************************************
-impl<V: NodeT> Graph<V, DirectedEdge, UnsignedEdge> {
-    pub fn new_directed_weighted() -> Self {
-        Self::new()
-    }
-}
-// ******************************************************************
-// **********          Directed, Signed Weights            **********
-// ******************************************************************
-impl<V: NodeT> Graph<V, DirectedEdge, SignedEdge> {
-    pub fn new_directed_weighted_signed() -> Self {
-        Self::new()
+impl<V: NodeT, E: EdgeT> Graph<V, E, Undir<V,E>> {
+    pub fn insert_edge(&mut self, e: E, l: &V, r: &V) -> Option<()> {
+        let ptr = self.create_edge(e, l, r)?;
+        self.nodes.get_mut(l)?.hood.neighbors.push(ptr);
+        self.nodes.get_mut(r)?.hood.neighbors.push(ptr);
+        Some(())
     }
 }
 
-// ******************************************************************
-// **********          Undirected, Unsigned Weights        **********
-// ******************************************************************
-impl<V: NodeT> Graph<V, UndirectedEdge, UnsignedEdge> {
-    pub fn new_undirected_weighted() -> Self {
-        Self::new()
+impl<V: NodeT, E: EdgeT> Graph<V, E, Dir<V,E>> {
+    pub fn insert_edge(&mut self, e: E, l: &V, r: &V) -> Option<()> {
+        let ptr = self.create_edge(e, l, r)?;
+        self.nodes.get_mut(l)?.hood.children.push(ptr);
+        self.nodes.get_mut(r)?.hood.parents.push(ptr);
+        Some(())
     }
 }
-
-// ******************************************************************
-// **********          Undirected, Signed Weights          **********
-// ******************************************************************
-impl<V: NodeT> Graph<V, UndirectedEdge, SignedEdge> {
-    pub fn new_undirected_weighted_signed() -> Self {
-        Self::new()
-    }
-}
-*/
-
-
-// hmmmm
-//  can I use one vec of edges and only store slices of it?
-//  every edge will be incident to 2 vertices (may be the same)
-//  dude i don't fucken know it's too late
-// todo
-//  unify letters used for Vertex data (T/V/N)
 
